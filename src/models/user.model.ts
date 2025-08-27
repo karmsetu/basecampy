6d8bd1ee132341aad591ea30b502dc96f35d71c2
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2025-08-27 09:05:57 +0530

    jwt user model hooks

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 const userSchema = new Schema(
   {
@@ -74,6 +76,41 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.isPasswordCorrect = async function (password: string) {
   return await Bun.password.verify(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+    },
+    process.env.ACCESS_TOKEN_SECRET!,
+    { expiresIn: process.env.ACCESS_EXPIRY },
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET!,
+    { expiresIn: process.env.REFRESH_EXPIRY },
+  );
+};
+
+userSchema.methods.generateTemporaryToken = function () {
+  const unHashedToken = crypto.randomBytes(20).toString("hex");
+
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(unHashedToken)
+    .digest("hex");
+
+  const tokenExpiry = Date.now() + 20 * 60 * 1000; // 20 min
+
+  return { unHashedToken, hashedToken, tokenExpiry };
 };
 
 export const User = mongoose.model("User", userSchema);
--- types/env.d.ts
+++ types/env.d.ts
@@ -5,6 +5,10 @@ declare namespace NodeJS {
     PORT?: string;
     CORS_ORIGIN?: string;
     MONGO_URI?: string;
+    ACCESS_TOKEN_SECRET?: string;
+    ACCESS_EXPIRY?: `${number}d`;
+    REFRESH_TOKEN_SECRET?: string;
+    REFRESH_EXPIRY?: `${number}d`;
     // Add your other variables here
   }
 }
