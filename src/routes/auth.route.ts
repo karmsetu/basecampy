eb6e20c2e0fd5f35b936215d2199ea136d0f9a5b
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2025-08-30 05:13:29 +0530

    routing

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,33 @@
 import { Router } from "express";
-import { login, logout, registerUser } from "../controllers/auth.controller";
+import {
+  changeCurrentPassword,
+  forgotPassword,
+  getCurrentUser,
+  login,
+  logout,
+  refreshAccessToken,
+  registerUser,
+  resetForgotPassword,
+  verifyEmail,
+  resendEmailVerification,
+} from "../controllers/auth.controller";
 import { verifyJWT } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(login);
+router.route("/verify-email/:verificationToken").get(verifyEmail);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/forgot-password").post(forgotPassword);
+router.route("/reset-password/:resetToken").post(resetForgotPassword);
+
+// secured routes
 router.route("/logout").post(verifyJWT, logout);
+router.route("/current-user").post(verifyJWT, getCurrentUser);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router
+  .route("/resend-email-verification")
+  .post(verifyJWT, resendEmailVerification);
 
 export default router;
