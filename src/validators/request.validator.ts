a28230bc0e40c6c917206644a700d4c3d13caf19
Karmsetu <141593846+karmsetu@users.noreply.github.com>
2025-08-30 05:13:52 +0530

    stash

--- src/validators/request.validator.ts
+++ src/validators/request.validator.ts
@@ -11,3 +11,16 @@ export const loginUserSchema = z.object({
   email: z.email().nonempty(),
   password: z.string().trim().nonempty().min(3),
 });
+
+export const userChangeCurrentPasswordSchema = z.object({
+  oldPassword: z.string().min(3).nonempty("Old password is required"),
+  newPassword: z.string().min(3).nonempty("New password is required"),
+});
+
+export const userForgotPasswordSchema = z.object({
+  email: z.email().nonempty("Email is required"),
+});
+
+export const userResetForgotPasswordSchema = z.object({
+  newPassword: z.string().min(3).nonempty("new password is required"),
+});
